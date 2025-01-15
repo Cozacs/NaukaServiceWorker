9884bd997dd848049f8c04c694393d53a3cd0f05
Cozac <victorcozac32@gmail.com>
2025-01-15 19:14:25 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,16 @@
-// service-worker.js
-self.addEventListener('install', function(event) {
-    console.log('Service Worker instalando.');
-    // Armazene arquivos importantes no cache para acesso offline
-    event.waitUntil(
-        caches.open('meu-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/script.js',
-                '/icon.png'
-            ]);
-        })
-    );
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log('Buscando:', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('push', function(event) {
-    const options = {
-        body: event.data.text(),
-        icon: '/icon.png',
-        badge: '/badge.png'
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Notificação Push', options)
-    );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
